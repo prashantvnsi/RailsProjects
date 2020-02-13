cf463c531a80e60f8d9a87e0628cb3dd7d011ebd
Prashant Tiwari <prashant.tiwari@blacklane.com>
2020-02-13 17:57:06 +0100

    Updating routes.rb to make devise work

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,7 @@
 class Users::SessionsController < Devise::SessionsController
-  def create
-  end
-
   def destroy
-    session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out!"
+    super
+    redirect_to 'https://google.com'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_for :users
   get 'welcome/index'
-
-  resources :sessions, only: [:create, :new, :destroy]
 
   resources :articles do
    resources :comments
